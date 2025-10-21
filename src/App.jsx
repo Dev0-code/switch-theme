3010f8a4097778ee047359966275901536a2a72f
Dev0-code <amalnathk03@gmail.com>
2025-10-21 15:28:42 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [dark,setDark] = useState(false)
+
+  const handletheme = () => {
+    setDark(!dark)
+  }
+  
+  return (
+    <>
+      <div
+      style={{
+        height:"100vh",
+        background : dark ? "black" : "white"
+      }}
+      className='d-flex flex-column align-items-center justify-content-center'
+      >
+        <h1 style={{color: dark ? "white" : "black"}}>Hello</h1>
+        <button className='btn' style={{color: dark ? "white" : "black"}} onClick={handletheme}>Change</button>
+      </div>
+    </>
+  )
+}
+
+export default App
